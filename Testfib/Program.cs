555a079c41fc2d84f3b113286451a3200f26c342
Golran <43411971+Golran@users.noreply.github.com>
2018-10-25 20:50:44 +0500

    Я сделал (#2)

--- Testfib/Program.cs
+++ Testfib/Program.cs
@@ -10,8 +10,9 @@ namespace Testfib
     {
         static void Main(string[] args)
         {
-            var number1 = 2;
-            var number2 = 2;
+            // Внёс вклад в развитие
+            var number1 = 1;
+            var number2 = 1;
             for (int i = 0; i < 30; i++)
             {
                 Console.WriteLine(number1);
